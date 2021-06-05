6e29f4e701b2d459134ffece415db800f2d13452
StavrosKoum <stkoum@gmail.com>
2021-06-05 21:40:18 +0300

    prog

--- Part3/jtb-javacc-2017/project_2/minijava_example/Example.ll
+++ Part3/jtb-javacc-2017/project_2/minijava_example/Example.ll
@@ -1,4 +1,7 @@
+Example
+class has this many methods-> 0
 A
+class has this many methods-> 2
 name->foo
 type->int
 int,int
@@ -6,6 +9,13 @@ name->bar
 type->int
 
 B
+class has this many methods-> 3
+name->foo
+type->int
+int,int
+name->bar
+type->int
+
 name->foobar
 type->boolean
 boolean
--- Part3/jtb-javacc-2017/project_2/minijava_example/LLVM_Gen.java
+++ Part3/jtb-javacc-2017/project_2/minijava_example/LLVM_Gen.java
@@ -26,7 +26,7 @@ public class LLVM_Gen
 
     public void create_vt() throws Exception
     {
-        int num = 1;
+        int num = 0;
         class_class value= null;
         while(num < this.class_counter) ///class while
         {
@@ -38,9 +38,20 @@ public class LLVM_Gen
                     System.out.println("-----"+value.class_name+"-----");
                     emit(value.class_name+"\n");
 
+                    int ex_meths_count = 0;
+                    if(value.ex_class != null)
+                        ex_meths_count = Table.get(value.ex_class).meth_count;
+
+                    int total_methods = ex_meths_count + value.meth_count;
+                    emit("class has this many methods-> "+total_methods+"\n");
 
 
-
+                    if(value.ex_class != null)
+                    {
+                        
+                        emit_extended_class_meth(value.ex_class);
+                    }
+                    
                     //methods here++++++++++++++++++++++++++++++++++++++++++++++
                     Method_class  meth_value = null;
                     int meth_num = 0;
@@ -98,7 +109,47 @@ public class LLVM_Gen
 
 
 
+public int emit_extended_class_meth(String ex_class) throws Exception
+{
+    class_class value= null;
+    value = Table.get(ex_class);
 
+    //methods here++++++++++++++++++++++++++++++++++++++++++++++
+    Method_class  meth_value = null;
+    int meth_num = 0;
+    while(meth_num < value.meth_count)
+    {
+        for(String key1: value.Methods_Table.keySet())
+        {
+        
+            meth_value = value.Methods_Table.get(key1);
+            if(meth_value.num_id == meth_num)
+            {
+                // value.print_all();
+                System.out.println(meth_value.method_name);
+                emit("name->"+meth_value.method_name+"\n");
+                //take meth info here
+                emit("type->"+meth_value.type+"\n");
+                //meth args++++++++++++++++++++++++++++++++++++++=
+                for(int i = 0; i <meth_value.args_list.size(); i ++)
+                {
+                    if(i == 0)
+                    emit(meth_value.args_list.get(i));
+                    else
+                    emit(","+meth_value.args_list.get(i));
+                }
+                emit("\n");
+
+
+                meth_num++;
+            }
+            
+            
+
+        }
+    }
+    return value.meth_count;
+}
 
 
 
