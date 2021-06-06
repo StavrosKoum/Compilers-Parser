3d806ab260f3744de137d864119cdf7e53115378
StavrosKoum <stkoum@gmail.com>
2021-06-06 17:06:17 +0300

    vt almost done

--- Part3/jtb-javacc-2017/project_2/minijava_example/Example.ll
+++ Part3/jtb-javacc-2017/project_2/minijava_example/Example.ll
@@ -1,21 +1,6 @@
-Example
-class has this many methods-> 0
-A
-class has this many methods-> 2
-name->foo
-type->int
-int,int
-name->bar
-type->int
 
-B
-class has this many methods-> 3
-name->foo
-type->int
-int,int
-name->bar
-type->int
-
-name->foobar
-type->boolean
-boolean
+@.Example_vtable = global [0 x i8*] [][]
+@.A_vtable = global [2 x i8*][i8* bitcast (i32 (i8*,i32,i32)* @A.foo to i8*i8* bitcast (i32 (i8*,i32,i32)* @A.foo to i8*,i8* bitcast (i32 (i8*,i8*)* @A.bar to i8*,i8* bitcast (i32 (i8*,i8*)* @A.bar to i8*]
+@.B_vtable = global [3 x i8*][i8* bitcast (i32 (i8*,i32,i32)* @A.foo to i8*
+,i8* bitcast (i32 (i8*,i8*)* @A.bar to i8*
+i8* bitcast (i1 (i8*,i1)* @B.foobar to i8*i8* bitcast (i1 (i8*,i1)* @B.foobar to i8*]
--- Part3/jtb-javacc-2017/project_2/minijava_example/LLVM_Gen.java
+++ Part3/jtb-javacc-2017/project_2/minijava_example/LLVM_Gen.java
@@ -1,7 +1,7 @@
 import syntaxtree.*;
 import visitor.*;
 import java.util.HashMap;
-
+import java.util.*;
 import javax.print.DocFlavor.STRING;
 import java.io.*;
 
@@ -36,16 +36,21 @@ public class LLVM_Gen
                 if(value.num_id == num)
                 {
                     System.out.println("-----"+value.class_name+"-----");
-                    emit(value.class_name+"\n");
+                    //emit(value.class_name+"\n");
 
                     int ex_meths_count = 0;
                     if(value.ex_class != null)
                         ex_meths_count = Table.get(value.ex_class).meth_count;
 
                     int total_methods = ex_meths_count + value.meth_count;
-                    emit("class has this many methods-> "+total_methods+"\n");
+                    //emit("class has this many methods-> "+total_methods+"\n");
+                    if(total_methods != 0)
+                        emit("\n@."+value.class_name+"_vtable = global ["+total_methods+" x i8*]");
+                    else
+                        emit("\n@."+value.class_name+"_vtable = global ["+total_methods+" x i8*] []");
 
-
+                    
+                    emit("[");
                     if(value.ex_class != null)
                     {
                         
@@ -55,28 +60,47 @@ public class LLVM_Gen
                     //methods here++++++++++++++++++++++++++++++++++++++++++++++
                     Method_class  meth_value = null;
                     int meth_num = 0;
+                    String emit_str;
                     while(meth_num < value.meth_count)
                     {
                         for(String key1: value.Methods_Table.keySet())
                         {
-                        
+                            
                             meth_value = value.Methods_Table.get(key1);
                             if(meth_value.num_id == meth_num)
                             {
-                                // value.print_all();
-                                System.out.println(meth_value.method_name);
-                                emit("name->"+meth_value.method_name+"\n");
-                                //take meth info here
-                                emit("type->"+meth_value.type+"\n");
-                                //meth args++++++++++++++++++++++++++++++++++++++=
-                                for(int i = 0; i <meth_value.args_list.size(); i ++)
+
+                                ///////////////////////////////////
+                                //////////////////////////////////
+                                if(meth_num == 0)
                                 {
-                                    if(i == 0)
-                                    emit(meth_value.args_list.get(i));
-                                    else
-                                    emit(","+meth_value.args_list.get(i));
+                                    emit_str = "i8* bitcast (" + give_types(meth_value.type) + " (" + give_args_types(meth_value.args_list) + ")* @" + value.class_name+"."+meth_value.method_name 
+                                    +" to i8*";
                                 }
-                                emit("\n");
+                                else
+                                {
+                                    emit_str = ",i8* bitcast (" + give_types(meth_value.type) + " (" + give_args_types(meth_value.args_list) + ")* @" + value.class_name+"."+meth_value.method_name 
+                                    +" to i8*";
+                                }
+                                //////////////////////////////////
+                                //////////////////////////////////
+                                emit(emit_str);
+
+
+                                // // value.print_all();
+                                // System.out.println(meth_value.method_name);
+                                // emit("name->"+meth_value.method_name+"\n");
+                                // //take meth info here
+                                // emit("type->"+meth_value.type+"\n");
+                                // //meth args++++++++++++++++++++++++++++++++++++++=
+                                // for(int i = 0; i <meth_value.args_list.size(); i ++)
+                                // {
+                                //     if(i == 0)
+                                //     emit(meth_value.args_list.get(i));
+                                //     else
+                                //     emit(","+meth_value.args_list.get(i));
+                                // }
+                                emit(emit_str);
                                
 
 
@@ -87,6 +111,8 @@ public class LLVM_Gen
 
                         }
                     }
+
+                    emit("]");
 
 
 
@@ -117,6 +143,7 @@ public int emit_extended_class_meth(String ex_class) throws Exception
     //methods here++++++++++++++++++++++++++++++++++++++++++++++
     Method_class  meth_value = null;
     int meth_num = 0;
+    String emit_str;
     while(meth_num < value.meth_count)
     {
         for(String key1: value.Methods_Table.keySet())
@@ -125,19 +152,40 @@ public int emit_extended_class_meth(String ex_class) throws Exception
             meth_value = value.Methods_Table.get(key1);
             if(meth_value.num_id == meth_num)
             {
-                // value.print_all();
-                System.out.println(meth_value.method_name);
-                emit("name->"+meth_value.method_name+"\n");
-                //take meth info here
-                emit("type->"+meth_value.type+"\n");
-                //meth args++++++++++++++++++++++++++++++++++++++=
-                for(int i = 0; i <meth_value.args_list.size(); i ++)
+
+
+
+
+
+
+                ///////////////////////////////////
+                //////////////////////////////////
+                if(meth_num == 0)
                 {
-                    if(i == 0)
-                    emit(meth_value.args_list.get(i));
-                    else
-                    emit(","+meth_value.args_list.get(i));
+                    emit_str = "i8* bitcast (" + give_types(meth_value.type) + " (" + give_args_types(meth_value.args_list) + ")* @" + value.class_name+"."+meth_value.method_name 
+                    +" to i8*";
                 }
+                else
+                {
+                    emit_str = ",i8* bitcast (" + give_types(meth_value.type) + " (" + give_args_types(meth_value.args_list) + ")* @" + value.class_name+"."+meth_value.method_name 
+                    +" to i8*";
+                }
+                //////////////////////////////////
+                //////////////////////////////////
+                emit(emit_str);
+                // // value.print_all();
+                // System.out.println(meth_value.method_name);
+                // emit("name->"+meth_value.method_name+"\n");
+                // //take meth info here
+                // emit("type->"+meth_value.type+"\n");
+                // //meth args++++++++++++++++++++++++++++++++++++++=
+                // for(int i = 0; i <meth_value.args_list.size(); i ++)
+                // {
+                //     if(i == 0)
+                //     emit(meth_value.args_list.get(i));
+                //     else
+                //     emit(","+meth_value.args_list.get(i));
+                // }
                 emit("\n");
 
 
@@ -149,6 +197,32 @@ public int emit_extended_class_meth(String ex_class) throws Exception
         }
     }
     return value.meth_count;
+}
+
+public String give_types(String type)
+{
+    if(type.equals("int"))
+        return "i32";
+
+    if(type.equals("boolean"))
+    return "i1";
+    
+    if(type.equals("int[]"))
+    return "i32*";
+
+    return "i8*";
+
+    
+}
+
+public String give_args_types(List<String> arguments)
+{
+    String a = "i8*";
+    for (int i = 0; i < arguments.size(); i++ )
+    {
+        a = a + "," + give_types(arguments.get(i));
+    }
+    return a;
 }
 
 
